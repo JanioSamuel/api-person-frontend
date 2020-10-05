2a8dc5baed672d49459d59fd45e82bfed5a5588a
Janio Samuel <samucagomes08@gmail.com>
2020-10-04 22:11:11 -0300

    created env file

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -34,11 +34,15 @@ export default function Navbar() {
         <Link to="/form" style={{ textDecoration: 'none' }}>
           <label>REGISTER PERSON</label>
         </Link>
-        <a href='https://api-person-backend.herokuapp.com/swagger-ui.html' target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+        <a href={process.env.NODE_ENV === 'development' ? 'http://localhost:8080/swagger-ui.html' : 'https://api-person-backend.herokuapp.com/swagger-ui.html'}
+          target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
           <label>DOCS</label>
         </a>
+        <a href="https://github.com/JanioSamuel/api-person-frontend" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+          <label>GITHUB FRONTEND</label>
+        </a>
         <a href="https://github.com/JanioSamuel/api-person-backend" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-          <label>GITHUB</label>
+          <label>GITHUB BACKEND</label>
         </a>
         <label onClick={handleLogout}>LOGOUT</label>
       </div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_URL_BACKEND,
+    baseURL: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_URL_BACKEND_LOCAL : process.env.REACT_APP_URL_BACKEND,
 });
 
 export default api;
